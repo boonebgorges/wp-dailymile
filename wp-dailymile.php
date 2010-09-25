cc028718ebdfce98b35cb0a6f4e6953ae339fa9b
Boone Gorges <boonebgorges@gmail.com>
2010-09-25 13:24:26 -0400

    Adding plugin loader file

---
+++ wp-dailymile.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: WP-Dailymile
+Plugin URI: http://teleogistic.net/code/wordpress/wp-dailymile
+Description: Changes your life
+Version: 0.1
+Author: Boone and Rebecca Gorges
+Author URI: 
+*/
+
+
+if ( !class_exists( 'WP_Dailymile_Loader' ) ) :
+
+class WP_Dailymile_Loader {
+
+	// PHP4 loader
+	function anthologize_loader () {
+		$this->__construct();
+	}
+	
+	function __construct() {
+		
+	}
+}
+
+endif;
+
+$wp_dailymile = new WP_Dailymile_Loader();
+
+
+/*
+App Details (edit)
+Callback URL
+http://teleogistic.net/code/wordpress/wp-dailymile
+Support URL
+http://teleogistic.net/code/wordpress/wp-dailymile
+OAuth Details
+Consumer Key
+yBNCmBLgnnFnn1MUfQbQ
+Consumer Secret
+KcdeyI8ceOVNtbx52P3qhUzv0rbnYB5apW6jRANM
+Request Token URL
+http://api.dailymile.com/oauth/request_token
+Access Token URL
+http://api.dailymile.com/oauth/access_token
+Authorize URL
+http://api.dailymile.com/oauth/authorize
+Supported Request Signing Functions
+HMAC-SHA1
+
+*/
